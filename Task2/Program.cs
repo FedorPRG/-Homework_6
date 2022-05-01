ba08d6c17818b74edbe8708a69a9cdead1f67022
Fedor <fedor_prg@rambler.ru>
2022-05-01 11:17:34 +1000

    Created Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Для уравнения y=k1*x+b1 введите значения:");
+Console.Write("b1=");
+double b1 = double.Parse(Console.ReadLine());
+Console.Write("k1=");
+double k1 = double.Parse(Console.ReadLine());
+Console.WriteLine("Для уравнения y=k2*x+b2 введите значения:");
+Console.Write("b2=");
+double b2 = double.Parse(Console.ReadLine());
+Console.Write("k2=");
+double k2 = double.Parse(Console.ReadLine());
+
+if (k1 == k2)
+{
+    Console.WriteLine($"k1=k2 -> {k1}={k2} -> прямые параллельны и не имеют точек пересечения");
+}
+else
+{
+    double x = (b2 - b1) / (k1 - k2);//
+    double y = k1 * x + b1;
+    Console.WriteLine($"Точка пересечения двух прямых имеют координаты х={x}; y={y}");
+}
